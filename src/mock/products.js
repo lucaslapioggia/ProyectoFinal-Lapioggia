95ac9f2a5c1fdddb8e57cfc6e8497cfdf32070e3
Lucas Lapioggia <73617414+lucaslapioggia@users.noreply.github.com>
2025-10-15 13:14:24 -0300

    products.js

---
+++ src/mock/products.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    "id":"p1",
+    "name":"Sillón Azul",
+    "description":"Sillón cómodo de 3 plazas",
+    "price":120000,
+    "stock":5,
+    "category":"tapiceria",
+    "image":""
+  },
+  {
+    "id":"p2",
+    "name":"Sillón Reclinable",
+    "description":"Reclinable con soporte lumbar",
+    "price":155000,
+    "stock":3,
+    "category":"tapiceria",
+    "image":""
+  },
+  {
+    "id":"p3",
+    "name":"Mesa Ratona",
+    "description":"Mesa de madera maciza",
+    "price":45000,
+    "stock":10,
+    "category":"carpinteria",
+    "image":""
+  }
+];
+export default products;
